33ed67227cee1f43861e3c0ce6241428fac1c316
Donghyun Kim <6moguai9@gmail.com>
2026-01-07 12:50:47 +0100

    Time: 0 ms (100%), Space: 19.5 MB (15.42%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -0,0 +1,17 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        letter_table = [
+            "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"
+        ]
+
+        result = []
+        def dfs(i, comb):
+            if i == len(digits):
+                result.append(comb)
+                return
+
+            for letter in letter_table[int(digits[i]) - 2]:
+                dfs(i + 1, comb + letter)
+        dfs(0, "")
+
+        return result
