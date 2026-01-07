86ef9160f5b897c851371a05888b91a278f6714b
Donghyun Kim <6moguai9@gmail.com>
2026-01-07 13:50:07 +0100

    Time: 0 ms (100%), Space: 19.4 MB (5.57%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
@@ -1,15 +1,12 @@
 class Solution:
     def tribonacci(self, n: int) -> int:
-        tri = deque([0, 1, 1])
+        tri = [0, 1, 1]
         
         if n < 3:
             return tri[n]
         
         total = 2
         for i in range(3, n):
-            tri.append(total)
-            total += total
-            total -= tri[0]
-            tri.popleft()
+            tri[0], tri[1], tri[2], total = tri[1], tri[2], total, total - tri[0] + total
 
         return total
