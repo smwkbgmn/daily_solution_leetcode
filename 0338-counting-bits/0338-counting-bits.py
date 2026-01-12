54d04e882299bfa73c256c801edc7397d36f27f6
Donghyun Kim <6moguai9@gmail.com>
2026-01-12 12:47:18 +0100

    Time: 0 ms (100%), Space: 21.2 MB (5.92%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -1,15 +1,10 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
         table = [0, 1, 1, 2]
-        if n < 4:
-            return table[:n+1]
 
         m = 4
         while m <= n:
-            for i in range(m):
-                table.append(table[i] + 1)
-                if len(table) == n + 1:
-                    break
+            table.extend([i + 1 for i in table])
             m *= 2
         
-        return table
+        return table[:n + 1]
