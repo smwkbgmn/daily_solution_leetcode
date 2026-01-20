8f14a6d1048b4e16ffb6134956f1df2280f4fee3
Donghyun Kim <6moguai9@gmail.com>
2026-01-20 11:30:13 +0100

    Time: 0 ms (100%), Space: 19.4 MB (17.67%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
@@ -1,23 +1,15 @@
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
-        (lng, shrt) = (str1, str2) if len(str1) > len(str2) else (str2, str1)
+        # Filter first if it has a common divider
+        if str1 + str2 != str2 + str1:
+            return ""
 
-        gcd = shrt
-        for i in range(len(shrt)):
-            if len(lng) % len(gcd) != 0 or len(shrt) % len(gcd) != 0:
-                gcd = gcd[:-1]
-                continue
-
-            j = 0
-            while j < len(lng) and lng[j] == gcd[j % len(gcd)]:
-                if j < len(shrt) and shrt[j] != gcd[j % len(gcd)]:
-                    break
-                j += 1
-            
-            if j == len(lng) and j % len(gcd) == 0:
-                break
-
-            gcd = gcd[:-1]
+        # Euclid gcd - looping instead of recurssion (just preference)
+        def gcd(n, m):
+            while m:
+                n, m = m, n % m
+            return n
         
-        return gcd
+        # Return the longest common divider 
+        return str1[:gcd(len(str1), len(str2))]
             
