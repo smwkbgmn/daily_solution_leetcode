d71bd0815160eb2f794e5307180274d7a71ebd43
Donghyun Kim <6moguai9@gmail.com>
2026-01-26 12:29:13 +0100

    Time: 0 ms (100%), Space: 19.3 MB (24.15%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -2,25 +2,19 @@ class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
         if len(s) == 0:
             return True
-        
+
         if len(t) == 0:
             return False
 
-        if len(s) == 1 and len(t) == 1:
+        if (len(s) == len(t) == 1):
             return s == t
-
-        table = [[0] * 26 for _ in range(len(t) + 1)]
-        for i, c in enumerate(t):
-            char_i = ord(c) - ord('a')
-            for j in reversed(range(i + 1)):
-                if table[j][char_i] != 0:
-                    break
-                table[j][char_i] = i + 1
-
-        prev = 0
-        for c in s:
-            char_i = ord(c) - ord('a')
-            if table[prev][char_i] <= prev:
-                return False
-            prev = table[prev][char_i]
-        return True 
+            
+        i = 0
+        for c in t:
+            if c == s[i]:
+                i += 1
+                if i == len(s):
+                    return True
+        
+        return False
+        
